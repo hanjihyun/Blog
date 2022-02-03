c1a3f1054adaba95c428026e5c6e65423c3f8568
한지현 <gksgpals96@naver.com>
2022-02-03 23:08:11 +0900

    Add : Vuetify

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,13 +5,15 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './plugins/element.js'
-import GAuth from 'vue-google-oauth2'
-Vue.use(BootstrapVue)
-Vue.use(GAuth, {clientId: '18503095621-5nr55k6ulpaigcjc129aundrhf4tldd6.apps.googleusercontent.com', scope: 'profile email https://www.googleapis.com/auth/plus.login'})
 
+
+import vuetify from './plugins/vuetify'
+
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
    router,
+    vuetify,
   render: h => h(App),
 }).$mount('#app')
---
+++ frontend/src/plugins/vuetify.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+    theme: {
+        dark: true,		// 어두운 테마 사용
+    },
+});
