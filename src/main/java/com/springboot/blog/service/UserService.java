2ab47313bb96cfa9234379cfc47c9f90ad2a91cd
한지현 <gksgpals96@naver.com>
2022-02-07 23:15:06 +0900

    Add : user Service

--- src/main/java/com/springboot/blog/service/UserService.java
+++ src/main/java/com/springboot/blog/service/UserService.java
@@ -1,4 +1,13 @@
 package com.springboot.blog.service;
 
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class UserService {
+    public String getUserInfo (String id_token) {
+        String email="";
+
+        return email;
+    }
 }
